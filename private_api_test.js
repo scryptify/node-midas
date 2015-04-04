6ed8b79bbd6e9be97196a37bc62f7e84e1b557e1
Blue-Dog-Archolite <Blue.Dog.Archolite@gmail.com>
2015-04-04 16:01:33 -0600

    Update for API calls

--- private_api_test.js
+++ private_api_test.js
@@ -1,5 +1,6 @@
 var sys = require('sys'),
-request = require('request');
+request = require('request'),
+crypto = require('crypto');
 
 // Load Procted Env file
 require('dotenv').load();
@@ -7,9 +8,26 @@ require('dotenv').load();
 var key = process.env.COINKITE_API_KEY;
 var secret = process.env.COINKITE_API_SECRET;
 
-sys.puts(key);
-sys.puts(secret);
-
 // Read https://github.com/coinkite/coinkite-javascript for help
-CK_API = require('./coinkite-api.js');
-console.log("Example:\n", CK_API.auth_headers('aa', 'bb', 'cc'));
+CK_API = require('./node_modules/coinkite/coinkite-api.js');
+var coin_kite_header = CK_API.auth_headers(key, secret, 'https://api.coinkite.com/v1/my/self');
+console.log("header:\n", coin_kite_header);
+
+
+var options = {
+  url: 'https://api.coinkite.com/v1/my/self',
+  headers: coin_kite_header
+};
+
+function callback(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var info = JSON.parse(body);
+    console.log(info.stargazers_count + " Stars");
+    console.log(info.forks_count + " Forks");
+  }else{
+    console.log('ERROR');
+    console.log(response.body);
+  }
+}
+
+request.get(options, callback);
